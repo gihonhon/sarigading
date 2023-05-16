e7c4454234e2a58e84903096a2c7d3c0e8268651
Gihhonn <11201001@student.itk.ac.id>
2023-05-17 01:07:57 +0800

    add banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+  subsets:['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={montserrat.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
