b02aeb6a0b20a688ee70ed1e9eb5fdf3e542795c
Gihhonn <11201001@student.itk.ac.id>
2023-05-24 09:50:17 +0800

    finish landing page, and add login page

---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import { Montserrat } from 'next/font/google'
+const montserrat = Montserrat({
+  subsets:['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main className={montserrat.className}>
+      <Component {...pageProps} />
+    </main>
+  ) 
+}
